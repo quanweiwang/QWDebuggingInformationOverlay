a37417121d835e50c8fac066b6ff55dbbd70a169
quanweiwang <629526514@qq.com>
2017-06-03 21:38:36 +0800

    QWDebuggingInformationOverlay
    QWDebuggingInformationOverlay

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QWDebuggingInformationOverlay
+//
+//  Created by 王权伟 on 2017/6/3.
+//  Copyright © 2017年 王权伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QWDebuggingInformationOverlay
+//
+//  Created by 王权伟 on 2017/6/3.
+//  Copyright © 2017年 王权伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
